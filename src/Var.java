6c4317f666aeb696d80efb737753db0cb7038728
LinusHoja <linushoja11@gmail.com>
2017-10-08 00:20:42 +0200

    Zeugs

--- src/Var.java
+++ src/Var.java
@@ -1,13 +1,26 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Var {
-	
-	public static JFrame jf1;		
+
+	public static JFrame jf1;
+	public static int contentPane;
+	public static JPanel mainMenuePanel, editorPanel;
 
 	public Var() {
-		
+
+		contentPane = 1;
+
+		// Panels 
+		mainMenuePanel = new MainMenue();
+		editorPanel = new EditorPanel();
+
 		jf1 = new Frame();
-		
+		if (contentPane == 0) {
+			jf1.setContentPane(mainMenuePanel);
+		} else if (contentPane == 1) {
+			jf1.setContentPane(editorPanel);
+		}
 	}
 
 }
