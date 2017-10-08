46dececba2d24e9c071aec51950e6c1632847f10
Moritz <skyblock@gmx.de>
2017-10-08 14:59:08 +0200

    Added Map file

---
+++ src/main/Map.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Map {
+
+    private int width, heigth;
+
+    public Map(int width, int heigth) {
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(int heigth) {
+        this.heigth = heigth;
+    }
+
+}
