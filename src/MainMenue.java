68de43e9dce968654c69f77871c95f8ca2d69b3e
LinusHoja <linushoja11@gmail.com>
2017-10-08 12:56:40 +0200

    BTNFix

--- src/MainMenue.java
+++ src/MainMenue.java
@@ -5,8 +5,9 @@ public class MainMenue extends JPanel{
 	private JButton btnEditor, btnExit;
 	
 	public MainMenue(){
+		setLayout(null);
 		btnEditor = new JButton("Editor");
-		btnEditor.setBounds(Var.width/2-100, 200, 200, 50);
+		btnEditor.setBounds(Var.width/2-100, 100, 200, 50);
 		btnEditor.setVisible(true);
 		
 		add(btnEditor);
