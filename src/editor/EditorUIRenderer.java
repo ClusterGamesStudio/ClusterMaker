62b227a044261aa06eaf5915c8af49792f34b3c8
Moritz <skyblock@gmx.de>
2017-10-08 13:57:17 +0200

    Added tool button templates

--- src/editor/EditorUIRenderer.java
+++ src/editor/EditorUIRenderer.java
@@ -8,8 +8,10 @@ public class EditorUIRenderer {
 
     private int iconSize;
     private int padding = 20;
+    private int toolLength;
     public EditorUIRenderer() {
         iconSize = Var.width/16;
+        toolLength = EditorPanel.EditorTool.values().length;
     }
 
     public void render() {
@@ -17,8 +19,8 @@ public class EditorUIRenderer {
     }
 
     public void draw(Graphics2D graphics2D) {
-        for(int i = 0; i<EditorPanel.EditorTool.values().length; i++) {
-            graphics2D.fillRect(padding, padd);
+        for(int i = 0; i<toolLength; i++) {
+            graphics2D.fillRect(padding, (i*padding+i*iconSize)+(toolLength*padding+toolLength*iconSize)/2+padding, iconSize, iconSize);
         }
     }
 
